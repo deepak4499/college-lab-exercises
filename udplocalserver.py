06e391f18efaf85e960506fa94e6e27700a543da
deepak4499 <40885656+deepak4499@users.noreply.github.com>
2018-07-06 14:24:20 +0530

    Create udplocalserver.py

---
+++ udplocalserver.py
@@ -0,0 +1,10 @@
+
+import socket
+UDP_IP_ADDRESS="127.0.0.1"
+UDP_PORT_NUMBER=6789
+serversock=socket.socket(socket.AF_INET,socket.SOCK_DGRAM)
+serversock.bind((UDP_IP_ADDRESS,UDP_PORT_NO))
+print("server is waiting")
+while True:
+ data,addr=serversock.recvfrom(1024)
+ print("message:",data)
